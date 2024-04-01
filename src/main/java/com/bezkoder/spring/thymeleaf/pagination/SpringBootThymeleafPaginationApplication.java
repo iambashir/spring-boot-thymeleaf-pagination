53410f886d947913b4d249fd7c4e46665eb7ef17
Bashir Hossain <74961832+iambashir@users.noreply.github.com>
2024-04-01 16:04:36 +0600

    First commit

--- src/main/java/com/bezkoder/spring/thymeleaf/pagination/SpringBootThymeleafPaginationApplication.java
+++ src/main/java/com/bezkoder/spring/thymeleaf/pagination/SpringBootThymeleafPaginationApplication.java
@@ -8,6 +8,7 @@ public class SpringBootThymeleafPaginationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootThymeleafPaginationApplication.class, args);
+		System.out.println("Project Started successfully ..........");
 	}
 
 }
